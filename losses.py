6be0fc3cf9d3248505408945a9c7211de6844fbb
AhmedNasr <ahmed.nasr9677@gmail.com>
2023-09-20 21:36:07 +0300

    add changes to EDR

--- losses.py
+++ losses.py
@@ -6,8 +6,10 @@ from loguru import logger
 from models.discriminator import STAGE1_Discriminator
 
 class EDR_Loss(nn.Module):
-    def __init__(self, n_fft=2048, hop_length=512):
+    def __init__(self, n_fft=512, hop_length=128, device="cuda"):
         super(EDR_Loss, self).__init__()
+
+        self.fs = 44_100
         self.n_fft = n_fft
         self.hop_length = hop_length
         win_length = n_fft
@@ -19,20 +21,71 @@ class EDR_Loss(nn.Module):
                             center=True,
                             pad_mode="reflect",
                             power=2.0,
-                        ).to(torch.device('cuda'))
+                        ).to(torch.device(device))
+        
+        self.center_frequencies = [16, 32, 64, 128, 256, 512, 1000, 2000, 4000] # octav freqs from 16 hz to 4k hz as described by paper
+
+
+    def compute_stft(self, x: torch.Tensor):
+        spec = self.stft_func(x)
+        return  torch.permute(spec, (0, 1, 3, 2))
+
+
+
+
+
+    def compute_energy_bands(self, S):
+        """
+        Compute the energy remaining in a set of octave frequency bands at time t.
+        
+        Args:
+            S (torch.Tensor): Spectrogram of the RIR with shape (b, c, T, F), 
+                            where T is the total number of time frames and F is the 
+                            number of frequency bins.
+            center_frequencies (list): List of center frequencies (in Hz) for the bands.
+            t (int): Time frame index.
+            
+        Returns:
+            sum_energy_bands (float): sum of energy values for each band.
+        """
+        F = S.shape[3]
+
+        nyquist_f = self.fs  / 2
+
+        bin_indices = torch.tensor([int(f * F / nyquist_f) for f in self.center_frequencies])
+
+  
+        # Compute the total energy in the frequency bin
+        total_energy_bin = torch.sum(torch.abs(S[:, :, :, bin_indices])**2, dim=2)
+            
+            
+        
+        return total_energy_bin
+           
+    
 
     def forward(self, x, y):
+
         # Compute STFT of the input signals
-        spec1 = self.stft_func(x) # torch.stft(x, n_fft=self.n_fft, hop_length=self.hop_length, return_complex=False, center=True)
-        spec2 = self.stft_func(y) # torch.stft(y, n_fft=self.n_fft, hop_length=self.hop_length, return_complex=False, center=True)
-            
+
+        spec1 = self.compute_stft(x) # torch.stft(x, n_fft=self.n_fft, hop_length=self.hop_length, return_complex=False, center=True)
+        spec2 = self.compute_stft(y) # torch.stft(y, n_fft=self.n_fft, hop_length=self.hop_length, return_complex=False, center=True)
+
+        
+        # logger.debug(f"tensor shapes: {x.shape} {y.shape}")
+  
         # logger.debug(f"spec shapes: {spec1.shape} {spec2.shape}")
 
-        # Compute mean squared error loss between the squared magnitude spectrograms
-        mse_loss = F.mse_loss(spec1, spec2)
-    
-        return mse_loss
+        edr1 = self.compute_energy_bands(spec1)
+        edr2 = self.compute_energy_bands(spec2)
 
+        # logger.debug(f"edr shapes: {edr1.shape} {edr2.shape}")
+
+        # Compute mean squared error loss between the squared magnitude EDRs
+
+        mse = F.mse_loss(edr1, edr2)
+    
+        return mse
 
 
 class MSE_Loss(nn.Module):
@@ -60,16 +113,22 @@ class CGAN_Loss(nn.Module):
 
 if __name__ == "__main__":
 
-    x = torch.randn(1, 1, 2330)
-    y = torch.randn(1, 1, 2330)
+    x = torch.randn(1, 1, 11_025).cuda()
+    y = torch.randn(1, 1, 11_025).cuda()
 
-    disc = STAGE1_Discriminator()
+    criterion = EDR_Loss()
 
-    d = disc(x)
+    loss = criterion(x, y)
 
-    criterion = CGAN_Loss()
 
-    loss = criterion(d)
+
+    # disc = STAGE1_Discriminator()
+
+    # d = disc(x)
+
+    # criterion = CGAN_Loss()
+
+    # loss = criterion(d)
 
     logger.info(f"loss: {loss}")
 
--- test.py
+++ test.py
@@ -1,50 +1,27 @@
 import numpy as np
-
-# path = "./data/IRs_HL00W.mat"
-
-
-# with h5py.File(path, 'r') as file:
-#     my_array = np.array(file["data"][:])
-#     # min_, max_= np.min(my_array), np.max(my_array)
-
-#     print(my_array.shape)
-
-import torch.nn as nn
-import torch
-
-import torch.nn as nn
+import torchaudio 
+from loguru import logger
 
 
-## test and experiments for the decoder design to estimate the needed samples
 
-class TransposeConvolutionDecoder(nn.Module):
-    def __init__(self):
-        super(TransposeConvolutionDecoder, self).__init__()
-        
-        # Define 5 transposed convolution layers
-        self.conv1 = nn.ConvTranspose1d(in_channels=1024, out_channels=512, kernel_size=5, stride=2, padding=16, output_padding=0, dilation=1)
-        self.conv2 = nn.ConvTranspose1d(in_channels=512, out_channels=256, kernel_size=5, stride=2, padding=32, output_padding=0, dilation=2)
-        self.conv3 = nn.ConvTranspose1d(in_channels=256, out_channels=128, kernel_size=7, stride=4, padding=74, output_padding=0, dilation=2)
-        self.conv4 = nn.ConvTranspose1d(in_channels=128, out_channels=64, kernel_size=7, stride=4, padding=141, output_padding=0, dilation=1)
-        self.conv5 = nn.ConvTranspose1d(in_channels=64, out_channels=1, kernel_size=5, stride=2, padding=6, output_padding=0, dilation=1)
-        
-    def forward(self, x):
-        x = self.conv1(x)
-        x = self.conv2(x)
-        x = self.conv3(x)
-        x = self.conv4(x)
-        x = self.conv5(x)
-        return x
 
-# Initialize the decoder
-decoder = TransposeConvolutionDecoder()
 
-# Input shape: (batch_size, channels, length)
-# Output shape: (batch_size, channels, length)
-# Example usage:
-import torch
+if __name__ == "__main__":
 
-input_tensor = torch.randn(1, 1024, 128)
-output_tensor = decoder(input_tensor)
+    data_path = "./data/"
 
-print("Output shape:", output_tensor.shape)
+    audio_file = data_path + "Accordion Solo_0_1sec.wav"
+    rir_file = data_path + "Accordion Solo_0.npy"
+
+    audio, sr = torchaudio.load(audio_file)
+
+    logger.info(f"audio: {audio.shape},  {sr}")
+    logger.info(f"audio tensor stats: {audio.mean(), audio.max(), audio.min()}")
+
+    rir = np.load(rir_file)
+
+    logger.info(f"rir: {rir.shape}")
+    logger.info(f"rir arr stats: {rir.mean(), rir.max(), rir.min()}")
+
+
+
